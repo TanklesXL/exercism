21ab23b1a8c8298b4a7a9a474d16dd29efddb4f6
Robert Attard <robert.attard@mail.mcgill.ca>
2020-08-04 10:03:41 -0400

    rust:space-age done with associated const

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -7,64 +7,48 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
-    fn orbital() -> f64;
+    const ORBITAL: f64;
     fn years_during(Duration(d): &Duration) -> f64 {
         let earth_seconds_per_year = (60 * 60 * 24) as f64 * 365.25;
-        d / earth_seconds_per_year / Self::orbital()
+        d / earth_seconds_per_year / Self::ORBITAL
     }
 }
 
 pub struct Mercury;
 impl Planet for Mercury {
-    fn orbital() -> f64 {
-        0.2408467
-    }
+    const ORBITAL: f64 = 0.2408467;
 }
 
 pub struct Venus;
 impl Planet for Venus {
-    fn orbital() -> f64 {
-        0.61519726
-    }
+    const ORBITAL: f64 = 0.61519726;
 }
 pub struct Earth;
 impl Planet for Earth {
-    fn orbital() -> f64 {
-        1.0
-    }
+    const ORBITAL: f64 = 1.0;
 }
 
 pub struct Mars;
 impl Planet for Mars {
-    fn orbital() -> f64 {
-        1.8808158
-    }
+    const ORBITAL: f64 = 1.8808158;
 }
 
 pub struct Jupiter;
 impl Planet for Jupiter {
-    fn orbital() -> f64 {
-        11.862615
-    }
+    const ORBITAL: f64 = 11.862615;
 }
 
 pub struct Saturn;
 impl Planet for Saturn {
-    fn orbital() -> f64 {
-        29.447498
-    }
+    const ORBITAL: f64 = 29.447498;
 }
 
 pub struct Uranus;
 impl Planet for Uranus {
-    fn orbital() -> f64 {
-        84.016846
-    }
+    const ORBITAL: f64 = 84.016846;
 }
 
 pub struct Neptune;
 impl Planet for Neptune {
-    fn orbital() -> f64 {
-        164.79132
-    }
+    const ORBITAL: f64 = 164.79132;
 }
