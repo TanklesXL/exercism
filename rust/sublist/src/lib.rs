b1e9aa9e18a3bbe7267d4e2c65a566438eeaa66c
Robert Attard <robert.attard@mail.mcgill.ca>
2020-09-21 16:43:26 -0400

    rust:sublist small order change

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,16 +7,15 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
-    let is_sublist = |little_list: &[T], big_list: &[T]| {
-        big_list
-            .windows(little_list.len())
-            .any(|window| window == little_list)
+    let is_sublist = |little_list: &[T], big_list: &[T]| match little_list.len() {
+        0 => true,
+        a => big_list.windows(a).any(|window| window == little_list),
     };
 
     match (first_list.len(), second_list.len()) {
-        (a, b) if a < b && (a == 0 || is_sublist(first_list, second_list)) => Comparison::Sublist,
-        (a, b) if a > b && (b == 0 || is_sublist(second_list, first_list)) => Comparison::Superlist,
         (a, b) if a == b && first_list == second_list => Comparison::Equal,
-        (_, _) => Comparison::Unequal,
+        (a, b) if a < b && is_sublist(first_list, second_list) => Comparison::Sublist,
+        (a, b) if a > b && is_sublist(second_list, first_list) => Comparison::Superlist,
+        _ => Comparison::Unequal,
     }
 }
